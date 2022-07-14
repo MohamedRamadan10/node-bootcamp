1281278bd2913e03d9732f05243ced5759117828
MohamedRamadan10 <mr.uiux.dev@gmail.com>
2022-07-14 22:31:54 +0200

    Asynchronous JavaScript Promises

---
+++ asynchronous-js/index.js
@@ -0,0 +1,34 @@
+const { rejects } = require('assert');
+const fs = require('fs');
+const { resolve } = require('path');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) reject('The is not found!!!');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file!!!');
+      resolve(data);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/data.txt`)
+  .then((data) => {
+    console.log(data);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(console.log('Random dog image save to file!!'))
+  .catch((err) => console.log(err));
